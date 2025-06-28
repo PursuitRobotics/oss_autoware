fad7445fd68026506e4a5a45153795a335ae599d
baseonballs: Jefrey Lucas <lucasjt@gmail.com>
2025-06-27 22:50:25 -0700

    Added overlays/scripts to support nos_setup.sh

---
+++ overlays/infra/scripts/nos_setup.sh
@@ -0,0 +1,19 @@
+# !/usr/bin/env bash
+
+
+git config --global user.name "baseonballs: Jeffrey Lucas"
+git config --global user.email "lucasjt@gmail.com"
+
+
+# install the pnpm package manager
+# and the fnm node version manager
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+curl -fsSL https://fnm.vercel.app/install | bash
+eval "$(fnm env --use-on-cd)"
+
+# install the latest LTS version of node
+fnm install --lts
+
+# and the python development tools
+sudo apt install -y python3 python3-pip python3-venv python3-dev build-essential make cmake
