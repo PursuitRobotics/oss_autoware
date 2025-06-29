7ef8d27d64da63934e3350d1b66b40eff1b9bcb4
baseonballs: Jeffrey Lucas <lucasjt@gmail.com>
2025-06-28 23:40:37 -0700

    Added updates to nos_setup.sh

--- overlays/infra/scripts/nos_setup.sh
+++ overlays/infra/scripts/nos_setup.sh
@@ -1,5 +1,27 @@
 # !/usr/bin/env bash
 
+export ROS_DISTRO=humble
+export ROS_DOMAIN_ID=1
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+
+export AW_DATA=/srv/autoware/autoware_data
+export AW_MAPS=/srv/autoware/autoware_maps
+
+function setup_ros2 {
+    local DIR=${HOME}/dev/pursuit/oss_autoware
+
+    export AW_BUILD=$DIR/build
+    export AW_LOGS=$DIR/log
+    export AW_INSTALL=$DIR/install
+}
+
+function setup_dirs() {
+    sudo mkdir -p ${AW_DATA}
+    sudo mkdir -p ${AW_MAPS}
+    
+    sudo chown ${USER}:${USER} ${AW_DATA}
+    sudo chown ${USER}:${USER} ${AW_MAPS}
+}
 
 git config --global user.name "baseonballs: Jeffrey Lucas"
 git config --global user.email "lucasjt@gmail.com"
